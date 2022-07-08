c74485afbfcbcd7e94811b61dd3ccde53a51916a
Sophia Ho <Sophia.HoMeiYin@softwire.com>
2022-07-08 10:17:22 +0100

    Can mark items as complete/not complete

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_items, add_item, get_item, save_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -14,11 +14,25 @@ def index():
 
 
 @app.route('/add', methods=['GET', 'POST'])
-def login():
+def add():
     if request.method == 'POST':
         title = request.form.get('title')
         add_item(title)
         return redirect(url_for('index'))
 
+@app.route('/complete', methods=['GET', 'POST'])
+def complete():
+    if request.method == 'POST':
+        item = get_item(request.form['completeBtn'])
+        item['status'] = "Complete"
+        save_item(item)
+        return redirect(url_for('index'))
 
+@app.route('/uncomplete', methods=['GET', 'POST'])
+def uncomplete():
+    if request.method == 'POST':
+        item = get_item(request.form['uncompleteBtn'])
+        item['status'] = "Not Started"
+        save_item(item)
+        return redirect(url_for('index'))
 
