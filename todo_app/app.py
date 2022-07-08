4e513af0b71c018d223794475b3fd1448094ed17
Sophia Ho <Sophia.HoMeiYin@softwire.com>
2022-07-08 09:15:42 +0100

    Displays to do items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +9,15 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template('index.html', items=items)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        print(title)
+        return '/'
+
+
